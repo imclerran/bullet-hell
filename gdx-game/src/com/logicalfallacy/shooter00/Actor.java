6fa91aaca16aa2dd692088ce0ce35731fd0e2757
Ian McLerran <imclerran@gmail.com>
2014-04-09 07:16:37 -0700

    Major bug fixes -- movement controls working

--- gdx-game/src/com/logicalfallacy/shooter00/Actor.java
+++ gdx-game/src/com/logicalfallacy/shooter00/Actor.java
@@ -18,7 +18,7 @@ public class Actor
 		_sprite.setScale(Gdx.graphics.getWidth()/64);
 		_sprite.setOrigin(_sprite.getWidth()/2, _sprite.getHeight()/2);
 		
-		_speed = 10000.0f;
+		_speed = 1000.0f;
 		_dxdy = new Vector2();
 		_dest = new Vector2();
 	}
@@ -42,15 +42,25 @@ public class Actor
 			// calculate x,y speeds
 			calcDxDy();
 			
-			//set sprite location
-			_sprite.setX(_sprite.getX() + _dxdy.x * Gdx.graphics.getDeltaTime());
-			_sprite.setY(_sprite.getY() + _dxdy.y * Gdx.graphics.getDeltaTime());
+			// set move distance
+			float movex = _dxdy.x * Gdx.graphics.getDeltaTime();
+			float movey = + _dxdy.y * Gdx.graphics.getDeltaTime();
+			
+			// check for over travel, then set new x
+			if(Math.abs(movex) < Math.abs(_sprite.getX() -  _dest.x))
+				_sprite.setX(_sprite.getX() + movex);
+			else
+				_sprite.setX(_dest.x);
+			
+			// check for over travel, then set new y
+			if(Math.abs(movey) < Math.abs(_sprite.getY() -  _dest.y))
+				_sprite.setY(_sprite.getY()  + movey);
+			else
+				_sprite.setY(_dest.y);
+				
 		}
 	}
 	
-	// TODO:
-	// increases in _speed do not appear to have equally increase effective speed
-	// speed appears greater greater while farther from destination
 	public void calcDxDy()
 	{
 		// calculate x, y distance from current position to destination
@@ -66,10 +76,10 @@ public class Actor
 		_dxdy.y = (_speed/dist)*disty;
 		
 		// correct for over travel
-		if(Math.abs(_dxdy.x) > Math.abs(distx))
+		/*if(Math.abs(_dxdy.x) > Math.abs(distx))
 			_dxdy.x = distx;
 		if(Math.abs(_dxdy.y) > Math.abs(disty))
-			_dxdy.y = disty;
+			_dxdy.y = disty;*/
 	}
 	
 	public void draw(Batch batch)
